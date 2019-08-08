f498603ae8d009462e6c68ed340657f3cda528a1
Jonathan Busch <72jon@Jonathans-MacBook-Pro-2.local>
2019-08-08 15:35:11 -0500

    still trying to get apollo working

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -4,16 +4,64 @@ import MongoDBInterface from '@accounts/mongo'
 import { AccountsPassword } from '@accounts/password'
 import { AccountsServer } from '@accounts/server'
 import { mergeResolvers, mergeTypeDefs } from 'graphql-toolkit'
-import db from './server'
+import db from '../server'
+
 import {
   GraphQLDate,
   GraphQLDateTime
 } from 'graphql-iso-date'
+import initialState from '../requests'
 const {
   ApolloServer,
   gql,
   makeExecutableSchema
 } = require('apollo-server-lambda')
+
+/*
+const typeDefs = gql`
+  scalar Date
+  scalar DateTime
+  type Query {
+    hello: String
+    getRequest(id: Int!): Request
+    getRequests: [Request]
+  }
+  type Request {
+      id: Int
+      title: String
+      start: DateTime
+      end: DateTime
+      salesman: String
+      amount: String
+      status: String
+      customers: [String]
+    }
+`
+
+const resolvers = {
+  Date: GraphQLDate,
+  DateTime: GraphQLDateTime,
+  Query: {
+    hello: (parent, args, context) => {
+      return 'Hello, world!'
+    },
+    getRequest: (obj, args, context) => {
+      const result = initialState.filter(result => result.id === args.id)[0]
+      return result
+    },
+    getRequests: () => {
+      return initialState
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+exports.handler = server.createHandler()
+*/
 
 const runHandler = (event, context, handler) => (
   new Promise((resolve, reject) => {
@@ -101,214 +149,6 @@ const run = async (event, context) => {
       customers: [String]
     }
   `
-  const today = new Date()
-  const endDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7)
-  const initialState = [
-    {
-      id: 0,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk',
-        'Merit Electric'
-      ]
-    },
-    {
-      id: 1,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Pending',
-      customers: [
-        'Fisk'
-      ]
-    },
-    {
-      id: 2,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Lost',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 3,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Won',
-      customers: [
-        'Britain Electric'
-      ]
-    },
-    {
-      id: 4,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Fisk'
-      ]
-    },
-    {
-      id: 5,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 6,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Fisk'
-      ]
-    },
-    {
-      id: 7,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric'
-      ]
-    },
-    {
-      id: 8,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 9,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 10,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 11,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 12,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 13,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 14,
-      title: 'My testing project name',
-      start: today,
-      end: endDate,
-      salesman: 'Jon Busch',
-      amount: '90,000.00',
-      status: 'Open',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    },
-    {
-      id: 15,
-      title: 'My testing project name that\'s is really really long',
-      start: today,
-      end: endDate,
-      salesman: 'Tex Tarango',
-      amount: '900.00',
-      status: 'Won',
-      customers: [
-        'Britain Electric',
-        'Fisk'
-      ]
-    }
-  ]
   const resolvers = {
     Date: GraphQLDate,
     DateTime: GraphQLDateTime,
